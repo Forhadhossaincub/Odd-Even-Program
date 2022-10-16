35cf3ecd7c0886062a81ff015aeed289ca824cf3
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 12:25:38 +0600

    Add project files.

---
+++ OddEven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd Even Program");
+
+            Console.Write("Enter a intergernumber : ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"You Enter {num} is Even Nuber ");
+            }
+            else
+            {
+                Console.WriteLine($"You Enter {num} is Odd Number ");
+            }
+
+
+            Console.ReadKey();
+          
+
+
+        }
+    }
+}
